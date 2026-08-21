d8e6a4390acbfd8d9a26251299b5a2d7d26be28e
RayanDEVR <samiulrayan18@gmail.com>
2026-08-21 21:34:51 +0600

    Stock Extremes

---
+++ Ex69stockExtremes.java
@@ -0,0 +1,32 @@
+/*
+Stock Extremes   [Build from Scratch | Intermediate]
+Find the smallest and largest stock quantity in a non-empty array and print their indexes. Start from the first 
+element rather than arbitrary sentinel values. 
+*/
+
+public class Ex69stockExtremes {
+    public static void main(String[] args) {
+        int[] stock = { 20, 30, 50, 13, 60 };
+        
+        int maxStock = stock[0];
+        int minStock = stock[0];
+        int minIndex = 0; 
+        int maxIndex = 0;
+
+        for (int i = 0; i < stock.length; i++) {
+            
+            if (maxStock < stock[i]) {
+                maxStock = stock[i];
+                maxIndex= i;
+            }
+            if (minStock > stock[i]) {
+                minStock = stock[i];
+                minIndex = i;
+            }
+
+        }
+        
+        System.out.println("Min Stock: " + minStock + " at Index " + minIndex);
+        System.out.println("Max Stock: " + maxStock + " at Index " + maxIndex);
+    }
+}
